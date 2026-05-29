c1463358bc12c5d238c7f5a6ea31c221c6afae2e
Administrator <hariprakash6383@gmail.com>
2026-05-29 19:47:24 +0530

    init commit

---
+++ protfolio/src/main/java/in/hariprakash/protfolio/ProtfolioApplication.java
@@ -0,0 +1,13 @@
+package in.hariprakash.protfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtfolioApplication.class, args);
+	}
+
+}
---
+++ protfolio/src/main/java/in/hariprakash/protfolio/controller/HomeController.java
@@ -0,0 +1,34 @@
+package in.hariprakash.protfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "","/home"})
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
+    }
+
+    @GetMapping("/projects")
+    public String showProjectPage(Model model) {
+        model.addAttribute("title", "Projects");
+        return "master";
+    }
+
+    @GetMapping("/resume")
+    public String showResumePage(Model model) {
+        model.addAttribute("title", "Resume");
+        return "master";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage(Model model) {
+        model.addAttribute("title", "Contact");
+        return "master";
+    }
+
+}
